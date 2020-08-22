11ad840392ac43a41dd2e98e407138c1185d99cf
Vasiliy Bannikov <vab-999@ya.ru>
2020-08-22 22:37:16 +0500

    ♻️ Some refactorings for procedural fizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+
 namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Precedural FizzBuzz 💩");
+            ProceduralFizzBuzz();
+        }
+
+        private static void ProceduralFizzBuzz()
+        {
+            const string buzz = "Buzz";
+            const string fizz = "Fizz";
+            const string fizzBuzz = fizz + buzz;
+            static bool NumberDividesBy(int n, int d) => n % d == 0;
             for (var i = 1; i <= 100; i++)
             {
-                if (i % 15 == 0)
-                    Console.WriteLine("FizzBuzz");
-                else if (i % 5 == 0)
-                    Console.WriteLine("Buzz");
-                else if (i % 3 == 0)
-                    Console.WriteLine("Fizz");
+                if (NumberDividesBy(i, 15))
+                    Console.WriteLine(fizzBuzz);
+                else if (NumberDividesBy(i, 5))
+                    Console.WriteLine(buzz);
+                else if (NumberDividesBy(i, 3))
+                    Console.WriteLine(fizz);
                 else
                     Console.WriteLine(i);
             }
